00f9e53d589bf92e14d5d934c78ab88e8bb1ec42
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-10-24 10:47:48 -0700

    added and styled buttons

--- src/components/PaginationButtons/index.js
+++ src/components/PaginationButtons/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const PaginationButtons = () => {
     return(
-        <>
-        we will be buttons. 
-        </>
+        <div className="d-flex my-3 justify-content-center">
+            <button className="btn btn-danger mx-1">prev.</button><button className="btn btn-danger mx-1">next.</button>
+        </div>
     );
 };
 
