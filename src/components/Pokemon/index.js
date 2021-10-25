d43f85c998cf4918b18ac00bc0a5eefdebe007d7
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-10-25 10:42:48 -0700

    all components here just need to make less ugly

--- src/components/Pokemon/index.js
+++ src/components/Pokemon/index.js
@@ -9,9 +9,15 @@ const Pokemon = ({ pokemon }) => {
     const sprites = data?.sprites;
     const imageUrl = sprites?.front_default;
     const statList = data?.stats;
-    const typeList = data?.types;
+    const typeListData = data?.types;
     const weight = data?.weight;
 
+    const typesArr = [];
+    typeListData?.forEach(typeData=> {
+        const type = typeData.type.name
+        typesArr.push(type)
+    })
+    console.log(typesArr)
     const toggleModal = () => {
         setOpenModal(!openModal);
     };
@@ -22,7 +28,6 @@ const Pokemon = ({ pokemon }) => {
         const statString = `${statName}: ${baseStat}`
         statsArr.push(statString);
     })
-    console.log(statsArr)
     return (
         <>
             <div className="card col-3" key={name}>
@@ -37,14 +42,17 @@ const Pokemon = ({ pokemon }) => {
                                 x
                             </button>
                         </div>
-                        <div class>
-
-                        </div>
                         <h5 className="card-title text-center">{name}</h5>
                         <img src={imageUrl} className="card-img w-75 justify-self-center" alt={name} />
                         <div className ="stats container card w-75 mb-2">
                             {statsArr && statsArr.map(stat => (
                                 <p className="text-center">{stat}</p>
+                            ))}
+                            {weight && (
+                                <p className ='text-center'>{weight} lbs</p>
+                            )}
+                            {typesArr && typesArr.map(type => (
+                                <p>{type}</p>
                             ))}
                         </div>
                     </section>
