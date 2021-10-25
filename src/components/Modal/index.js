e1ab1678b8c7f3d7a9d827c4bac80ce75036ce7a
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-10-25 09:25:42 -0700

    card currently not ugly

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,17 +3,19 @@ import './styles.css'
 
 const Modal = ({ show, pokemonName, stats, types, weight, image }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
-  console.log(show)
+
   return (
     <div className={showHideClassName}>
-      <section className="modal-main">
-        <div className="card col-3" key={pokemonName}>
+      <section className="modal-main card w-25 row">
+        <div className="d-flex justify-content-end">
+          <button type="button" className="col-1 btn">
+            x
+          </button>
+        </div>
+        <div className="" key={pokemonName}>
           <img src={image} className="card-img-top" alt={pokemonName} />
           <h5 className="card-title text-center">{pokemonName}</h5>
         </div>
-        <button type="button">
-          x
-        </button>
       </section>
     </div>
   );
